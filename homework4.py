2683a7f7a5e38e09d53238845c8cacd88ccabd02
vladdavis2416 <sockolow.vladisl@gmail.com>
2024-07-14 17:39:04 +0300

    Add files via upload

---
+++ homework4.py
@@ -0,0 +1,7 @@
+my_string = (input("Введите текст: "))
+lenth = len(my_string)
+print(lenth)
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(" ", ""))
+print(my_string[-1])
